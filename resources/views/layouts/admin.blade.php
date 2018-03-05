635167611886a851f1e168570082a37f7d7ba90e
MadalinGavrila <madalin.gavrila13@yahoo.com>
2018-03-05 21:56:23 +0200

    portfolio finish

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>@yield('title')</title>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Portfolio Madalin Gavrila">
+    <meta name="author" content="Madalin Gavrila">
+
+    <title>@yield('title')</title>
 
     <!-- CSS -->
     <link rel="stylesheet" href="{{asset('css/libs-admin.css')}}">
--- resources/views/layouts/other.blade.php
+++ resources/views/layouts/other.blade.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>@yield('title')</title>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Portfolio Madalin Gavrila">
+    <meta name="author" content="Madalin Gavrila">
+
+    <title>@yield('title')</title>
 
     <!-- CSS -->
     <link rel="stylesheet" href="{{asset('css/libs.css')}}">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
-@yield('styles')
+    @yield('styles')
 
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
